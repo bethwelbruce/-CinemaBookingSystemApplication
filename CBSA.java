ac61fff049a0f84dec6171724e7af8607f42a6b4
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2023-05-19 13:26:48 +0300

    Create CBSA.java

---
+++ CBSA.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBookingSystemApplication.class, args);
+    }
+
+}
